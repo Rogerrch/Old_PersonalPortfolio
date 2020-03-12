496a1fadcd104674f215a65b382743475e37e9ff
RogerRCh <Rogerrch@gmail.com>
2020-03-12 02:29:03 -0600

    starship JS  built

--- starships/main.js
+++ starships/main.js
@@ -1,20 +1,41 @@
-import {starships} from '../data/starships.js'
+import { starships } from "../data/starships.js"
+import { removeChildren, getLastNumber } from '../utils.js'
 
-const nav = document.querySelector('.nav')
+const nav = document.querySelector(".nav")
 
-const navList = document.querySelector('.navList')
+const navList = document.querySelector(".navList")
+
+const shipView = document.querySelector('#main')
 
 function populateNav(starships) {
-    starships.forEach(starship => {
-        let shipAnchor = document.createElement('a')
-        let listItem = document.createElement('li')
-        listItem.textContent = starship.name
+  starships.forEach(starship => {
+    let shipAnchor = document.createElement("a")
+    shipAnchor.href = "#"
+    let listItem = document.createElement("li")
+    listItem.textContent = starship.name;
 
-        shipAnchor.appendChild(listItem)
-        navList.appendChild(shipAnchor)
+shipAnchor.addEventListener('click', event => {
+    //store the name of the list item clicked on
+    let shipName = event.target.textContent
+    const foundShip = starships.find(ship => ship.name === shipName)
+    console.log(foundShip)
+    populateShipView(foundShip)
+})
 
-    });
-    nav.appendChild(navList)
+    shipAnchor.appendChild(listItem)
+    navList.appendChild(shipAnchor)
+
+    })
+
+  nav.appendChild(navList);
+}
+
+function populateShipViwe(shipData);
+removeChildren(shipView)
+let imageNum = getLastNumber(shipData.url)
+let shipImage = document.createElement('img')
+shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${imageNum}.jpg`
+shipView.appendChild(shipImage)
 }
 
 populateNav(starships)
