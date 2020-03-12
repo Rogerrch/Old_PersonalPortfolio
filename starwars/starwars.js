a49849764462628db9731d3c041b674f1cc28e1e
RogerRCh <Rogerrch@gmail.com>
2020-03-11 21:39:51 -0600

    JS people list working

--- starwars/starwars.js
+++ starwars/starwars.js
@@ -1,41 +1,20 @@
-import { films } from "../data/films.js";
-import { films } from "../data/people.js";
-import { films } from "../data/starships";
-import { films } from "../data/species";
+import { films } from '../data/films.js'
+import { people } from '../data/people.js'
 
 console.log("Hi There! My first JavaScript code!")
 
 console.log(document.querySelector('.greeting'))
 
-let greetingDiv = document.gallerySelector('.greeting')
+let greetingDiv = document.querySelector('.greeting')
 
-let castList = document.captureEvents("ul")
+greetingDiv.textContent = "Star Wars Cast"
 
-//let counter = 1
+let castList = document.createElement("ul")
 
-people.forEach(person => {
+people.forEach((person) => {
 let nameItem = document.createElement("li")
 nameItem.textContent = person.name
 castList.appendChild(nameItem)
-
-let personAnchor = document.createElement("a")
-personAnchor.href = "#"
-let personImg = document.createElement("img")
-personImg.scr = `https://starwars-visualguide.com/assets/img/characters/(${counter}.jpg)`
-
-personImg.addEventListener('error', (event) => {
-  personImg.hidden = true
-  //personImg.src = '../images/uvu.jpeg'
-})
-
-personImg.addEventListener("click", function( event ) {
-  console.log('Thanks for clicking!')
-})
-
-personAnchor.appendChild(personImg)
-greetingDiv.appendChild(personAnchor)
-conter++
 })
 
 greetingDiv.appendChild(castList)
-
