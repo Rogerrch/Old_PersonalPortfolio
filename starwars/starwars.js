af0ad62d1883b53332c8f70bc334301a2ef17b6d
RogerRCh <Rogerrch@gmail.com>
2020-03-11 22:12:16 -0600

    JS counter command added

--- starwars/starwars.js
+++ starwars/starwars.js
@@ -11,13 +11,17 @@ greetingDiv.textContent = "Star Wars Cast"
 
 let castList = document.createElement("ul")
 
+let counter = 1
+
 people.forEach((person) => {
   let nameItem = document.createElement("li")
   nameItem.textContent = person.name
   castList.appendChild(nameItem)
   let personImg = document.createElement("img")
   personImg.src = `https://starwars-visualguide.com/assets/img/characters/${counter}.jpg`
+
   greetingDiv.appendChild(personImg)
+  counter++
 })
 
 greetingDiv.appendChild(castList)
