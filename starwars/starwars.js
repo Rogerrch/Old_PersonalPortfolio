b358c71dcab1fa6b6bc0977144586c876542a96f
RogerRCh <Rogerrch@gmail.com>
2020-03-12 00:12:06 -0600

    Sort setup for genders

--- starwars/starwars.js
+++ starwars/starwars.js
@@ -1,41 +1,51 @@
 import { films } from '../data/films.js'
 import { people } from '../data/people.js'
 
-console.log("Hi There! My first JavaScript code!")
-
-console.log(document.querySelector('.greeting'))
-
 let greetingDiv = document.querySelector('.greeting')
 
-// greetingDiv.textContent = "Star Wars Cast"
+const maleCharacters = people.filter(person => person.gender === 'male')
+console.log(maleCharacters)
 
-let castList = document.createElement("ul")
+const femaleCharacters = people.filter(person => person.gender === 'female')
+console.log(femaleCharacters)
+
+const otherCharacters = people.filter(
+  person =>
+    person.gender === 'n/a' ||
+    person.gender === 'mech' ||
+    person.gender === 'hermaphrodite',
+)
+console.log(otherCharacters)
+
+let maleButton = document.querySelector('#maleButton')
+let femaleButton = document.querySelector('#femaleButton')
+let otherButton = document.querySelector('#otherButton')
+
+maleButton.addEventListener('click', function(event) {
+  populateDOM(maleCharacters)
+})
 
 let counter = 1
 
+function populateDOM(characters) {
 people.forEach((person) => {
-  let nameItem = document.createElement("li")
-  nameItem.textContent = person.name
-  castList.appendChild(nameItem)
-
-  let personAnchor = document.createElement("a")
-  personAnchor.href ="#"
-  
+  console.log(`${person.url}`)
+  let personAnchor = document.createElement('a')
+  personAnchor.href ='#'
   let personImg = document.createElement("img")
   personImg.src = `https://starwars-visualguide.com/assets/img/characters/${counter}.jpg`
    
-  personImg.addEventListener('error', ( event ) => { 
+  personImg.addEventListener('error', event => { 
     personImg.hidden = true
   //  personImg.src = '../images/sm/SithDeathMark_sm.jpg'
-   });
+   })
 
-   personImg.addEventListener("click", function( event ) { 
-    console.log('tahnks for clicking')
+   personImg.addEventListener("click", function(event) { 
+    console.log('Thanks for clicking')
    })
  
   personAnchor.appendChild(personImg)
   greetingDiv.appendChild(personAnchor)
   counter++
 })
-
-greetingDiv.appendChild(castList)
+}
